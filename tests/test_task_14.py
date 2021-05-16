42813d5af79c954e37cbbd638b52594044784cde
1xdeadman <daniilZakharchenko@gmail.com>
2021-05-16 18:49:52 +1000

    - fix error

--- tests/test_task_14.py
+++ tests/test_task_14.py
@@ -5,7 +5,7 @@ import tasks.task_14 as task_14
 
 @pytest.mark.task_14
 def test_fizz_buzz():
-    assert "Buzz" == task_14.fizz_buzz(6)
-    assert "Fizz" == task_14.fizz_buzz(5)
-    assert "Fizz" == task_14.fizz_buzz(10)
+    assert "Fizz" == task_14.fizz_buzz(6)
+    assert "Buzz" == task_14.fizz_buzz(5)
+    assert "Buzz" == task_14.fizz_buzz(10)
     assert "FizzBuzz" == task_14.fizz_buzz(15)
